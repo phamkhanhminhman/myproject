ac83f316ee051d40485e1effa1d2a9dae431d1dd
phamminhman10 <phamkhanhminhman97@gmail.com>
2019-09-15 09:34:17 +0700

    Redirect Oauth2

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Alert;
 use DB;
-Use Alert;
+use Google\Auth\Credentials\UserRefreshCredentials;
+use Google\Auth\OAuth2;
+use Google\Photos\Library\V1\PhotosLibraryClient;
+use Google_Client;
 use Illuminate\Http\Request;
 use Intervention\Image\ImageManagerStatic as Image;
-use Google\Auth\Credentials\UserRefreshCredentials;
-use Google\Photos\Library\V1\PhotosLibraryClient;
-use Google\Photos\Library\V1\PhotosLibraryResourceFactory;
-use Google\Auth\OAuth2;
-use Google_Client; 
 
 class testController extends Controller {
 
@@ -41,7 +40,7 @@ class testController extends Controller {
 
 	public function import(Request $request) {
 
-		$file = $request->file('file'); 
+		$file = $request->file('file');
 		$data = getimagesize($file);
 
 		$width = $data[0];
@@ -53,80 +52,112 @@ class testController extends Controller {
 		// $file->move('./images/resize', $name);
 		$file->move('./images/', $name);
 
-
 		$import = DB::table('test')->insert(
 			[
-				'name' => 'images/'.'resize'. $file->getClientOriginalName(),
+				'name' => 'images/' . 'resize' . $file->getClientOriginalName(),
 				'size' => $size,
 				'username' => $request->session()->get('username'),
 			]);
 
-		//Resize 
+		//Resize
 		if ($width < 2000 || $height < 2000) {
-			$img = Image::make('images/'.$file->getClientOriginalName())->resize($width/2, $height/2);
-			$img->save(public_path('./images/resize' .$file->getClientOriginalName()));
+			$img = Image::make('images/' . $file->getClientOriginalName())->resize($width / 2, $height / 2);
+			$img->save(public_path('./images/resize' . $file->getClientOriginalName()));
 		} else {
-			$img = Image::make('images/'.$file->getClientOriginalName())->resize($width/4, $height/4);
-			$img->save(public_path('./images/resize' .$file->getClientOriginalName()));
+			$img = Image::make('images/' . $file->getClientOriginalName())->resize($width / 4, $height / 4);
+			$img->save(public_path('./images/resize' . $file->getClientOriginalName()));
 		}
-		alert('Uploaded','Successfully', 'success');
-		
+		alert('Uploaded', 'Successfully', 'success');
+
 	}
 
-	public function getListAlbums(Request $request)
-	{
+	public function getListAlbums(Request $request) {
 		$clientId = '701132481011-u3nq1lfn7aenv3svjab0dobqe2se7tot.apps.googleusercontent.com';
 		$clientSecret = '8lrvysKx4KEnJz_dPI2OWtxM';
-
 
 		$scopes = [
 			'https://www.googleapis.com/auth/photoslibrary',
 			'https://www.googleapis.com/auth/photoslibrary.readonly',
-			'https://www.googleapis.com/auth/photoslibrary.readonly.appcreateddat'
+			'https://www.googleapis.com/auth/photoslibrary.readonly.appcreateddat',
 
 		];
 
-		$oauth2 = new OAuth2([
-			'clientId' => $clientId,
-			'clientSecret' => $clientSecret,
-			'authorizationUri' => 'https://accounts.google.com/o/oauth2/v2/auth',
-            // Where to return the user to if they accept your request to access their account.
-            // You must authorize this URI in the Google API Console.
-			'redirectUri' => 'http://localhost:8000',
-			'tokenCredentialUri' => 'https://www.googleapis.com/oauth2/v4/token',
-			'scope' => $scopes,
-		]);
-		$code = $request->getQueryParam('code', $default = null);
-		dd($code);
-		$oauth2->setCode($code);
-		$authToken = $oauth2->fetchAuthToken();
-		dd($authToken);
+		$client = new Google_Client(); // khởi tạo Google Client
+		$client->setApplicationName('Demo Laravel'); // đặt tên cho ứng dụng (không quan trọng lắm)
+
+		// cài đặt xin quyền truy cập của token, ở đây tôi đang xin truy cập đến dữ liệu Calendar và Drive
+		$client->setScopes(\Google_Service_Drive::DRIVE_METADATA_READONLY);
+		$client->setAuthConfig('./ClientID - PKMM.json'); // đường dẫn đến file credentials.json
+		$client->setAccessType('offline'); // không rõ lắm nên cứ để vậy đi :">
+
+		$auth_url = $client->createAuthUrl();
+		// header('Location: http://www.freetuts.net/');
+
+		header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+		dd('1');
+
+			
+		// return $auth_url;
 
 
-		// require_once 'C:\Users\MinhMan\Documents\myproject\vendor\autoload.php';
-		$authCredentials =  new UserRefreshCredentials(
-			$scopes,
-			[
-				'client_id' => '701132481011-u3nq1lfn7aenv3svjab0dobqe2se7tot.apps.googleusercontent.com',
-				'client_secret' => '8lrvysKx4KEnJz_dPI2OWtxM',
-				'refresh_token' => 'ya29.Glt9B1poEHhc_fbkStcQKKngq6Zqi0xBeKap6jPD1yqF-Y8nTQ_hMId60Vkn-7avE3EJZ6XB2k41eUggjvCEpSEwIPfSyXF7F39QB8S0mPKrLXI9L2cXF2hSHd1N'
-			]
-		);
-		// dd($authCredentials);
-		$photosLibraryClient = new PhotosLibraryClient(['credentials' => $authCredentials]);
-		$response = $photosLibraryClient->listSharedAlbums();
-		dd($response);
-		foreach ($response->iterateAllElements() as $item) {
-			$id = $item->getId();
-			// $description = $item->getDescription();
-			// $mimeType = $item->getMimeType();
-			$productUrl = $item->getProductUrl();
-			// $filename = $item->getFilename();
-			dd($productUrl);
-		}
 
 
-		$client = new Google_Client();
-		dd($client);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// $oauth2 = new OAuth2([
+		// 	'clientId' => $clientId,
+		// 	'clientSecret' => $clientSecret,
+		// 	'authorizationUri' => 'https://accounts.google.com/o/oauth2/v2/auth',
+		// 	// Where to return the user to if they accept your request to access their account.
+		// 	// You must authorize this URI in the Google API Console.
+		// 	'redirectUri' => 'http://localhost:8000',
+		// 	'tokenCredentialUri' => 'https://www.googleapis.com/oauth2/v4/token',
+		// 	'scope' => $scopes,
+		// ]);
+
+		// dd($oauth2->getAccessToken());
+
+		// $code = $request->getQueryParam('code', $default = null);
+		// dd($code);
+		// $oauth2->setCode($code);
+		// $authToken = $oauth2->fetchAuthToken();
+		// dd($authToken);
+
+		// // require_once 'C:\Users\MinhMan\Documents\myproject\vendor\autoload.php';
+		// $authCredentials = new UserRefreshCredentials(
+		// 	$scopes,
+		// 	[
+		// 		'client_id' => '701132481011-u3nq1lfn7aenv3svjab0dobqe2se7tot.apps.googleusercontent.com',
+		// 		'client_secret' => '8lrvysKx4KEnJz_dPI2OWtxM',
+		// 		'refresh_token' => 'ya29.Glt9B1poEHhc_fbkStcQKKngq6Zqi0xBeKap6jPD1yqF-Y8nTQ_hMId60Vkn-7avE3EJZ6XB2k41eUggjvCEpSEwIPfSyXF7F39QB8S0mPKrLXI9L2cXF2hSHd1N',
+		// 	]
+		// );
+		// // dd($authCredentials);
+		// $photosLibraryClient = new PhotosLibraryClient(['credentials' => $authCredentials]);
+		// $response = $photosLibraryClient->listSharedAlbums();
+		// dd($response);
+		// foreach ($response->iterateAllElements() as $item) {
+		// 	$id = $item->getId();
+		// 	// $description = $item->getDescription();
+		// 	// $mimeType = $item->getMimeType();
+		// 	$productUrl = $item->getProductUrl();
+		// 	// $filename = $item->getFilename();
+		// 	dd($productUrl);
+		// }
+
+		// $client = new Google_Client();
+		// dd($client);
 	}
 }
